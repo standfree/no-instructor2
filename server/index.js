fe43214636a476a871a49b365b3d58febca2383e
howdycrystalm <howdycrystalm@gmail.com>
2018-06-16 14:51:54 -0700

    added comments

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
+// REQUIRE DEPENDENCIES
+// ============================================================
 var express = require('express');
 var massive = require('massive');
-var app = module.exports = express();
 var config = require('./config');
-// var connectionString = "postgres://ckerstiens:@localhost/example";
 
-// var db = massive.connectSync({connectionString : connectionString});
+// INITILIZE APP
+// ============================================================
+var app = module.exports = express();
 
-// db.users.find(1, function(err,res){
-//   console.log(res);
-// });
+// // INITILIZE DEPENDENCIES
+// // ============================================================
+// app.use(bodyParser.json());
 
+// VARIABLES
+// ============================================================
+var connectionString = config.MASSIVE_URI;
+
+// LISTEN
+// ============================================================
 app.listen(config.serverPort, function() {
   console.log('listening on port', config.serverPort);
 })
