016f1fd988f3059cfa127a9b4d3632e611918912
howdycrystalm <howdycrystalm@gmail.com>
2018-06-23 14:01:11 -0700

    established static routes

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const massive = require('massive');
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
+const path = require('path');
 
 // CONFIG
 // ============================================================
@@ -13,8 +14,9 @@ const config = require('./config');
 // INITILIZE APP (invoking Express)
 // ============================================================
 
-const app = module.exports = express();
-
+// const app = module.exports = express(); 
+const app = express();
+// app.use(express.static(__dirname));
 // app.use(express.static(__dirname + 'client'));
 app.use(express.static('client'));
 app.use(bodyParser.json());
@@ -25,25 +27,29 @@ app.use(cors());
 
 const connectionString = config.MASSIVE_URI;
 
-massive(connectionString).then( db => {
+massive(connectionString).then(db => {
   app.set('db', db);
-})
-
+});
 
 // USER ENDPOINTS
 // ============================================================
-// app.get('api/users', function(req, res, next) {
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/views', 'index.html'));
+});
+
+app.get('/resources', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/views', 'resources.html'));
+});
 
 app.get('/survivor-stories', (req, res, next) =>
-  req.app.get('db').get_all_users().then( users => {
+  req.app.get('db').get_all_users().then(users => {
     res.status(200).send(users);
   })
-)
-
-app.get('/', (req, res) => res.send('Hello World!'))
+);
 
 // LISTEN
 // ============================================================
 app.listen(config.serverPort, function() {
   console.log('express server listening on port', config.serverPort);
-})
+});
