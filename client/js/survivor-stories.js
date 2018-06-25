a6dac28465a23392382f563ed86af52be2964ed6
howdycrystalm <howdycrystalm@gmail.com>
2018-06-24 19:34:37 -0700

    data rendering on frontend form javascript file

--- client/js/survivor-stories.js
+++ client/js/survivor-stories.js
@@ -1,13 +1,17 @@
 const xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'http://localhost:2000/api/survivor-stories', true);
+
+xhr.open('GET', 'http://localhost:2000/api/survivor-stories', true); //opens connection
 
 xhr.onload = function() {
   if (this.status === 200) {
   console.log(JSON.parse(this.responseText));
+  document.getElementById('testID').innerHTML = this.responseText;
+
   } else {
     console.log('error');
+    document.getElementById('testID').innerHTML = '404 Not Found';
   }
 }
 
-xhr.send();
+xhr.send(); //closes connection
